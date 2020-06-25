5a869e652cceba4baa0e930739707774a67b26aa
Daniel Richards <daniel.richards@automattic.com>
2020-06-25 14:55:03 +0800

    Extract block draggable scroll behaviour into React hook

--- packages/block-editor/src/components/block-draggable/index.js
+++ packages/block-editor/src/components/block-draggable/index.js
@@ -4,34 +4,11 @@
 import { Draggable } from '@wordpress/components';
 import { useSelect, useDispatch } from '@wordpress/data';
 import { useEffect, useRef } from '@wordpress/element';
-import { getScrollContainer } from '@wordpress/dom';
 
-const SCROLL_INACTIVE_DISTANCE_PX = 50;
-const SCROLL_INTERVAL_MS = 25;
-const PIXELS_PER_SECOND_PER_DISTANCE = 5;
-const VELOCITY_MULTIPLIER =
-	PIXELS_PER_SECOND_PER_DISTANCE * ( SCROLL_INTERVAL_MS / 1000 );
-
-function startScrollingY( nodeRef, velocityRef ) {
-	return setInterval( () => {
-		if ( nodeRef.current && velocityRef.current ) {
-			const newTop = nodeRef.current.scrollTop + velocityRef.current;
-
-			nodeRef.current.scroll( {
-				top: newTop,
-				// behavior: 'smooth' // seems to hurt performance, better to use a small scroll interval
-			} );
-		}
-	}, SCROLL_INTERVAL_MS );
-}
-
-function getVerticalScrollParent( node ) {
-	if ( node === null ) {
-		return null;
-	}
-
-	return getScrollContainer( node );
-}
+/**
+ * Internal dependencies
+ */
+import useScrollWhenDragging from './use-scroll-when-dragging';
 
 const BlockDraggable = ( {
 	children,
@@ -65,12 +42,12 @@ const BlockDraggable = ( {
 	);
 	const isDragging = useRef( false );
 
-	// @todo - do this for horizontal scroll
-	const dragStartY = useRef( null );
-	const velocityY = useRef( null );
-	const scrollParentY = useRef( null );
-
-	const scrollEditorInterval = useRef( null );
+	const blockElementId = `block-${ clientIds[ 0 ] }`;
+	const [
+		startScrolling,
+		scrollOnDragOver,
+		stopScrolling,
+	] = useScrollWhenDragging( blockElementId );
 
 	const { startDraggingBlocks, stopDraggingBlocks } = useDispatch(
 		'core/block-editor'
@@ -82,11 +59,6 @@ const BlockDraggable = ( {
 			if ( isDragging.current ) {
 				stopDraggingBlocks();
 			}
-
-			if ( scrollEditorInterval.current ) {
-				clearInterval( scrollEditorInterval.current );
-				scrollEditorInterval.current = null;
-			}
 		};
 	}, [] );
 
@@ -94,7 +66,6 @@ const BlockDraggable = ( {
 		return children( { isDraggable: false } );
 	}
 
-	const blockElementId = `block-${ clientIds[ 0 ] }`;
 	const transferData = {
 		type: 'block',
 		srcIndex: index,
@@ -110,44 +81,19 @@ const BlockDraggable = ( {
 			onDragStart={ ( event ) => {
 				startDraggingBlocks();
 				isDragging.current = true;
-				dragStartY.current = event.clientY;
 
-				// find nearest parent(s) to scroll
-				scrollParentY.current = getVerticalScrollParent(
-					document.getElementById( blockElementId )
-				);
-				scrollEditorInterval.current = startScrollingY(
-					scrollParentY,
-					velocityY
-				);
+				startScrolling( event );
+
 				if ( onDragStart ) {
 					onDragStart();
 				}
 			} }
-			onDragOver={ ( event ) => {
-				const distanceY = event.clientY - dragStartY.current;
-				if ( distanceY > SCROLL_INACTIVE_DISTANCE_PX ) {
-					velocityY.current =
-						VELOCITY_MULTIPLIER *
-						( distanceY - SCROLL_INACTIVE_DISTANCE_PX );
-				} else if ( distanceY < -SCROLL_INACTIVE_DISTANCE_PX ) {
-					velocityY.current =
-						VELOCITY_MULTIPLIER *
-						( distanceY + SCROLL_INACTIVE_DISTANCE_PX );
-				} else {
-					velocityY.current = 0;
-				}
-			} }
+			onDragOver={ scrollOnDragOver }
 			onDragEnd={ () => {
 				stopDraggingBlocks();
 				isDragging.current = false;
-				dragStartY.current = null;
-				scrollParentY.current = null;
 
-				if ( scrollEditorInterval.current ) {
-					clearInterval( scrollEditorInterval.current );
-					scrollEditorInterval.current = null;
-				}
+				stopScrolling();
 
 				if ( onDragEnd ) {
 					onDragEnd();
